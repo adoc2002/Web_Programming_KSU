1df904b34df2dac012004c13e91dab9178561abd
Your Name <asmit452@kent.edu>
2022-06-16 19:31:52 -0400

    minor change

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask import render_template
 app= Flask(__name__)
 
-@app.route("/")
+@app.route("/")           
 def get_index():
     return "<p>Hello there from my GitHub repo!</p>"
     
 
 @app.route("/hello")   
 def get_hello():
-    return render_template('hello.html')
+    return render_template('hello.html', name = "Art")
+
+
+@app.route("/charisse")   
+def get_charisse():
+    return render_template('hello.html', name = "Charisse")
